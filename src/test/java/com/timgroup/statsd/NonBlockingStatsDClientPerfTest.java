adf3cc5adc59c49954fc65dd182a913f532c05be
Massimiliano Pippi <massi@datadoghq.com>
2016-08-26 14:57:15 +0200

    added support for CI

--- src/test/java/com/timgroup/statsd/NonBlockingStatsDClientPerfTest.java
+++ src/test/java/com/timgroup/statsd/NonBlockingStatsDClientPerfTest.java
@@ -6,8 +6,8 @@ import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -17,17 +17,17 @@ public final class NonBlockingStatsDClientPerfTest {
 
     private static final int STATSD_SERVER_PORT = 17255;
     private static final Random RAND = new Random();
-    private final NonBlockingStatsDClient client = new NonBlockingStatsDClient("my.prefix", "localhost", STATSD_SERVER_PORT);
+    private static final NonBlockingStatsDClient client = new NonBlockingStatsDClient("my.prefix", "localhost", STATSD_SERVER_PORT);
     private final ExecutorService executor = Executors.newFixedThreadPool(20);
-    private DummyStatsDServer server;
+    private static DummyStatsDServer server;
 
-    @Before
-    public void start() throws SocketException {
+    @BeforeClass
+    public static void start() throws SocketException {
         server = new DummyStatsDServer(STATSD_SERVER_PORT);
     }
 
-    @After
-    public void stop() throws Exception {
+    @AfterClass
+    public static void stop() throws Exception {
         client.stop();
         server.close();
     }
