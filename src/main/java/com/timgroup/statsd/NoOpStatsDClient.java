37fbddf3657e6cb70148d1235cb9c95130fc1d8b
Debraj Manna <debrajiitb@gmail.com>
2016-08-26 18:21:52 +0530

    Add sampleRate while submitting metrics to datadog (#11)
    Refer
    [Issue-9](https://github.com/DataDog/java-dogstatsd-client/issues/9)
    for more

--- src/main/java/com/timgroup/statsd/NoOpStatsDClient.java
+++ src/main/java/com/timgroup/statsd/NoOpStatsDClient.java
@@ -8,22 +8,37 @@ package com.timgroup.statsd;
  *
  */
 public final class NoOpStatsDClient implements StatsDClient {
-    @Override public void stop() { }
+	@Override public void stop() { }
     @Override public void count(String aspect, long delta, String... tags) { }
+    @Override public void count(String aspect, long delta, double sampleRate, String... tags) { }
     @Override public void incrementCounter(String aspect, String... tags) { }
+    @Override public void incrementCounter(String aspect, double sampleRate, String... tags) { }
     @Override public void increment(String aspect, String... tags) { }
+    @Override public void increment(String aspect, double sampleRate, String...tags) { }
     @Override public void decrementCounter(String aspect, String... tags) { }
+    @Override public void decrementCounter(String aspect, double sampleRate, String... tags) { }
     @Override public void decrement(String aspect, String... tags) { }
+    @Override public void decrement(String aspect, double sampleRate, String... tags) { }
     @Override public void recordGaugeValue(String aspect, double value, String... tags) { }
+    @Override public void recordGaugeValue(String aspect, double value, double sampleRate, String... tags) { }
     @Override public void gauge(String aspect, double value, String... tags) { }
+    @Override public void gauge(String aspect, double value, double sampleRate, String... tags) { }
     @Override public void recordGaugeValue(String aspect, long value, String... tags) { }
+    @Override public void recordGaugeValue(String aspect, long value, double sampleRate, String... tags) { }
     @Override public void gauge(String aspect, long value, String... tags) { }
+    @Override public void gauge(String aspect, long value, double sampleRate, String... tags) { }
     @Override public void recordExecutionTime(String aspect, long timeInMs, String... tags) { }
+    @Override public void recordExecutionTime(String aspect, long timeInMs, double sampleRate, String... tags) { }
     @Override public void time(String aspect, long value, String... tags) { }
+    @Override public void time(String aspect, long value, double sampleRate, String... tags) { }
     @Override public void recordHistogramValue(String aspect, double value, String... tags) { }
+    @Override public void recordHistogramValue(String aspect, double value, double sampleRate, String... tags) { }
     @Override public void histogram(String aspect, double value, String... tags) { }
+    @Override public void histogram(String aspect, double value, double sampleRate, String... tags) { }
     @Override public void recordHistogramValue(String aspect, long value, String... tags) { }
+    @Override public void recordHistogramValue(String aspect, long value, double sampleRate, String... tags) { }
     @Override public void histogram(String aspect, long value, String... tags) { }
+    @Override public void histogram(String aspect, long value, double sampleRate, String... tags) { }
     @Override public void recordEvent(final Event event, final String... tags) { }
     @Override public void recordServiceCheckRun(ServiceCheck sc) { }
     @Override public void serviceCheck(ServiceCheck sc) { }
