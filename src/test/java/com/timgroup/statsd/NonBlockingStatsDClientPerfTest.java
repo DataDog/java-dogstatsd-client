63c572ec6e2d3c37b9bc8aa5478bde274352734e
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2021-09-15 16:26:52 -0500

    [client] enable aggregation by default

--- src/test/java/com/timgroup/statsd/NonBlockingStatsDClientPerfTest.java
+++ src/test/java/com/timgroup/statsd/NonBlockingStatsDClientPerfTest.java
@@ -8,6 +8,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import java.util.Random;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -23,7 +24,15 @@ public final class NonBlockingStatsDClientPerfTest {
         .hostname("localhost")
         .port(STATSD_SERVER_PORT)
         .blocking(true)  // non-blocking processors will drop messages if the queue fills up
+        .enableAggregation(false)
         .build();
+
+    private static final NonBlockingStatsDClient clientAggr = new NonBlockingStatsDClientBuilder().prefix("my.prefix.aggregated")
+        .hostname("localhost")
+        .port(STATSD_SERVER_PORT)
+        .blocking(true)  // non-blocking processors will drop messages if the queue fills up
+        .build();
+
     private final ExecutorService executor = Executors.newFixedThreadPool(10);
     private static DummyStatsDServer server;
 
@@ -38,6 +47,11 @@ public final class NonBlockingStatsDClientPerfTest {
     public static void stop() throws Exception {
         client.stop();
         server.close();
+    }
+
+    @After
+    public void clear() throws Exception {
+        server.clear();
     }
 
     @Test(timeout=30000)
@@ -69,5 +83,43 @@ public final class NonBlockingStatsDClientPerfTest {
         log.info("Packets at server: " + server.packetsReceived());
 
         assertEquals(testSize, server.messagesReceived().size());
+    }
+
+    @Test(timeout=30000)
+    public void perfAggregatedTest() throws Exception {
+
+        int expectedSize = 2;
+        long start = System.currentTimeMillis();
+        boolean done = false;
+
+        while(!done) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    clientAggr.count("mycount", 1);
+                }
+            });
+
+            long elapsed = System.currentTimeMillis() - start;
+            if  (elapsed > clientAggr.statsDProcessor.getAggregator().getFlushInterval()) {
+                done = true;
+            }
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(20, TimeUnit.SECONDS);
+
+        int messages;
+        while((messages = server.messagesReceived().size()) < expectedSize) {
+
+            log.info("Messages at server: " + messages);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException ex) {}
+        }
+
+        log.info("Messages at server: " + messages);
+        log.info("Packets at server: " + server.packetsReceived());
+
+        assertEquals(expectedSize, server.messagesReceived().size());
     }
 }
