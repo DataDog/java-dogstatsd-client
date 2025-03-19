b0062ccbf72142f57413eaf456e4d0f4f69b9331
Sarah Chen <sarah.chen@datadoghq.com>
2025-03-19 10:46:11 -0400

    Revert changes made to DummyStatsDServer files

--- src/test/java/com/timgroup/statsd/UnixDatagramSocketDummyStatsDServer.java
+++ src/test/java/com/timgroup/statsd/UnixDatagramSocketDummyStatsDServer.java
@@ -3,7 +3,6 @@ package com.timgroup.statsd;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
-import java.net.SocketAddress;
 import jnr.unixsocket.UnixDatagramChannel;
 import jnr.unixsocket.UnixSocketAddress;
 
@@ -11,46 +10,23 @@ import static com.timgroup.statsd.NonBlockingStatsDClient.DEFAULT_UDS_MAX_PACKET
 
 public class UnixDatagramSocketDummyStatsDServer extends DummyStatsDServer {
     private final DatagramChannel server;
-    private volatile boolean serverRunning = true;
 
     public UnixDatagramSocketDummyStatsDServer(String socketPath) throws IOException {
-        if (ClientChannelUtils.hasNativeUdsSupport()) {
-            try {
-                Class<?> udsAddressClass = Class.forName("java.net.UnixDomainSocketAddress");
-                Object udsAddress = udsAddressClass.getMethod("of", String.class).invoke(null, socketPath);
-
-                DatagramChannel nativeServer = DatagramChannel.open();
-                nativeServer.bind((SocketAddress) udsAddress);
-                this.server = nativeServer;
-            } catch (ReflectiveOperationException e) {
-                throw new IOException(e);
-            }
-        } else {
-            UnixDatagramChannel jnrServer = UnixDatagramChannel.open();
-            jnrServer.bind(new UnixSocketAddress(socketPath));
-            this.server = jnrServer;
-        }
+        server = UnixDatagramChannel.open();
+        server.bind(new UnixSocketAddress(socketPath));
         this.listen();
     }
 
     @Override
     protected boolean isOpen() {
-        return serverRunning && server.isOpen();
+        return server.isOpen();
     }
 
     protected void receive(ByteBuffer packet) throws IOException {
         server.receive(packet);
     }
 
-    @Override
     public void close() throws IOException {
-        serverRunning = false;
-        try {
-            // Give the listening thread a chance to stop
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            // ignore
-        }
         try {
             server.close();
         } catch (Exception e) {
