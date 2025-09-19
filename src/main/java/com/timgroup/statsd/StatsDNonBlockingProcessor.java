5f19a244c24f24877530abc4f775f2a82b5b288f
ntroutman <nathanieltroutman@gmail.com>
2025-09-19 08:35:06 -0700

    Use ArrayBlockingQueue in StatsDNonBlockingProcessor
    Swaps out the ConcurrentLinkedQueue for ArrayBlockingQueue which is bounded
    by default removing the need to track queue size independently and avoiding
    allocation of linked nodes, instead perfering a constant upfront allocation
    reducing GC churn.

--- src/main/java/com/timgroup/statsd/StatsDNonBlockingProcessor.java
+++ src/main/java/com/timgroup/statsd/StatsDNonBlockingProcessor.java
@@ -1,21 +1,18 @@
 package com.timgroup.statsd;
 
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class StatsDNonBlockingProcessor extends StatsDProcessor {
 
     private final Queue<Message> messages;
-    private final AtomicInteger qsize; // qSize will not reflect actual size, but a close estimate.
 
     private class ProcessingTask extends StatsDProcessor.ProcessingTask {
         @Override
         protected Message getMessage() throws InterruptedException {
             final Message message = messages.poll();
             if (message != null) {
-                qsize.decrementAndGet();
                 return message;
             }
 
@@ -50,8 +47,7 @@ public class StatsDNonBlockingProcessor extends StatsDProcessor {
                 aggregatorFlushInterval,
                 aggregatorShards,
                 threadFactory);
-        this.qsize = new AtomicInteger(0);
-        this.messages = new ConcurrentLinkedQueue<>();
+        this.messages = new ArrayBlockingQueue<>(qcapacity);
     }
 
     @Override
@@ -62,11 +58,7 @@ public class StatsDNonBlockingProcessor extends StatsDProcessor {
     @Override
     protected boolean send(final Message message) {
         if (!shutdown) {
-            if (qsize.get() < qcapacity) {
-                messages.offer(message);
-                qsize.incrementAndGet();
-                return true;
-            }
+            return messages.offer(message);
         }
 
         return false;
